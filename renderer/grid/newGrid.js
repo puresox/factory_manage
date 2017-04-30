2c6a350c2a4971bb8ef306fad46d037e58d33592
puresox <13004384112@163.com>
2017-04-30 22:19:45 +0800

    litter

---
+++ renderer/grid/newGrid.js
@@ -0,0 +1,30 @@
+const Datastore = require('nedb');
+const {ipcRenderer} = require('electron')
+
+const db = new Datastore({
+    filename: '/db/score1',
+    autoload: true
+});
+$(document).ready(() => {
+    $('#save').click(() => {
+        const year = $("#year").val();
+        const month = $("#month").val();
+        const A = $("#A").text();
+        const B = $("#B").text();
+        const C = $("#C").text();
+        const D = $("#D").text();
+        const E = $("#E").text();
+        const data={
+            year:year,
+            month:month,
+            A:A,
+            B:B,
+            C:C,
+            D:D,
+            E:E,
+        };
+        db.insert(data,(err, newDoc)=>{
+            ipcRenderer.send('save', 'ping')
+        });
+    });
+});
